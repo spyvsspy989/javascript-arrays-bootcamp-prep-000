cf478fbd468c496d03e79a55f61264d0fcffb4ba
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 07:02:11 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -115,7 +115,6 @@ expect(chocolateBars).to.equal;
     it('alters the original array', () => {
       const array = [1]; 
     });
-    }));
  ("accessElementInArray(array, index)", () => {
     it("accesses the element in array at the given index", () => {
       expect
